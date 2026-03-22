857d97afc8094d65c65ba4cccc388f4dfd700ac8
heiko750 <122236675+heiko750@users.noreply.github.com>
2026-03-22 22:58:51 +0100

    Update run.sh

--- wetteronline/run.sh
+++ wetteronline/run.sh
@@ -1,17 +1,6 @@
-#!/usr/bin/env bash
-echo "Starting WetterOnline Scraper Add-on..."
+#!/usr/bin/with-contenv bashio
 
-LOCATION=$(jq -r '.location' /data/options.json)
-INTERVAL=$(jq -r '.interval' /data/options.json)
-MQTT_HOST=$(jq -r '.mqtt_host' /data/options.json)
-MQTT_PORT=$(jq -r '.mqtt_port' /data/options.json)
+bashio::log.info "Starting WetterOnline scraper..."
 
-export LOCATION
-export INTERVAL
-export MQTT_HOST
-export MQTT_PORT
+python3 /usr/src/app/scraper.py
 
-while true; do
-    python3 /scraper.py
-    sleep $((INTERVAL * 60))
-done
